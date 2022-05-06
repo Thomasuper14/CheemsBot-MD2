bd66f8ce8dd9ae8cc22cd1905b1f8845f7f0f2a5
Thomasuper14 <75450804+Thomasuper14@users.noreply.github.com>
2022-05-06 15:57:07 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
-global.botname = 'Cheems Bot MD V2'
+global.owner = ['39347037227']
+global.premium = ['393477037227']
+global.ownernomer = '393477037227'
+global.ownername = '🦄Hypnos'
+global.botname = 'Hypnos Bot V2'
 global.footer = 'Xeon Bot Inc.'
 global.ig = 'https://github.com/DGXeon'
 global.region = 'India, NorthEast, Mizoram'
 global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
 global.myweb = 'https://youtu.be/imFIX-Wrt3s'
 global.packname = 'Cheems Bot'
-global.author = '🦄Dream Guy Xeon'
+global.author = '🦄Hypnos'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
